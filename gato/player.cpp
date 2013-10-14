17c635a4cd20624f90004a3536f3826d7f1dadf3
abcsds <abcsds@gmail.com>
2013-10-14 14:16:48 -0500

    Ready for AI

--- gato/player.cpp
+++ gato/player.cpp
@@ -55,56 +55,12 @@ int player::computerMove(board *b) {
     int score;
     return max(b,&score);    
 }
-// TODO Check minimax
+// TODO minimax
 int player::max(board *b, int* score) {
-//    int bestMove = 0;
-//    int bestScore = 0;
-//    int newScore = 0;
-//    list<int> freeSpaces = b->getFreeSpaces();
-//    int n[freeSpaces.size()];
-//    for (list<int>::iterator it = freeSpaces.begin(); it != freeSpaces.end(); it++) {
-//        for (int j = 0; j <= freeSpaces.size() ; j++) {
-//            n[j] = *it;
-//        }
-//    }
-//    for (int i = 0; i <= freeSpaces.size() ; i++ ) {
-//        b->put(i, playerN);
-//        if (b->checkOver()) newScore = getScore(b);
-//        else min(b, &newScore);
-//        b->undoLastMove();
-//        if (newScore > bestScore) {
-//            bestMove = i;
-//            bestScore = newScore;
-//        }
-//        
-//    }
-//    *score = bestScore;
-//    return bestMove;
+    return 0;
 }
 int player::min(board *b, int* score) {
-//    int bestMove = 0;
-//    int bestScore = 0;
-//    int newScore = 0;
-//    list<int> freeSpaces = b->getFreeSpaces();
-//    int n[freeSpaces.size()];
-//    for (list<int>::iterator it = freeSpaces.begin(); it != freeSpaces.end(); it++) {
-//        for (int j = 0; j <= freeSpaces.size() ; j++) {
-//            n[j] = *it;
-//        }
-//    }
-//    for (int i = 0; i <= freeSpaces.size() ; i++ ) {
-//        b->put(i, oponentN);
-//        if (b->checkOver()) newScore = getScore(b);
-//        else max(b, &newScore);
-//        b->undoLastMove();
-//        if (newScore < bestScore) {
-//            bestMove = i;
-//            bestScore = newScore;
-//            cerr << i << endl;
-//        }
-//    }
-//    *score = bestScore;
-//    return bestMove;
+    return 0;
 }
 int player::getScore(board *b) {
     if (b->checkOver()) {
